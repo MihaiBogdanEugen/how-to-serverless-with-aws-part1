f3c56226b737ffa72df30ccd82c4991454a50d0b
Bogdan Mihai <mihai.bogdan.eugen@gmail.com>
2020-03-04 19:04:19 +0100

    Update MoviesDynamoDbRepository.java (#1)

--- java11/movies/functions/update-movie-rating/src/main/java/de/mbe/tutorials/aws/serverless/movies/functions/updatemovierating/repository/MoviesDynamoDbRepository.java
+++ java11/movies/functions/update-movie-rating/src/main/java/de/mbe/tutorials/aws/serverless/movies/functions/updatemovierating/repository/MoviesDynamoDbRepository.java
@@ -1,31 +1,32 @@
 package de.mbe.tutorials.aws.serverless.movies.functions.updatemovierating.repository;
 
+import static com.amazonaws.util.StringUtils.isNullOrEmpty;
+
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
 import de.mbe.tutorials.aws.serverless.movies.models.MovieRating;
 
-import static com.amazonaws.util.StringUtils.isNullOrEmpty;
-
 public final class MoviesDynamoDbRepository {
 
     private final DynamoDBMapper dynamoDBMapper;
     private final String movieRatingsTable;
+    private final DynamoDBMapperConfig writeToMovieRatingConfig;
 
     public MoviesDynamoDbRepository(final AmazonDynamoDB amazonDynamoDB, final String movieRatingsTable) {
         this.dynamoDBMapper = new DynamoDBMapper(amazonDynamoDB);
         this.movieRatingsTable = movieRatingsTable;
+
+        final var writeToMovieRatingConfigBuilder = DynamoDBMapperConfig.builder()
+            .withSaveBehavior(DynamoDBMapperConfig.SaveBehavior.UPDATE);
+
+        if (!isNullOrEmpty(this.movieRatingsTable)) {
+            writeToMovieRatingConfigBuilder.withTableNameOverride(new DynamoDBMapperConfig.TableNameOverride(this.movieRatingsTable));
+        }
+        writeToMovieRatingConfig = writeToMovieRatingConfigBuilder.build();
     }
 
     public void updateMovieRating(final MovieRating movieRating) {
-
-        final var writeToMovieRatingConfig = DynamoDBMapperConfig.builder()
-                .withSaveBehavior(DynamoDBMapperConfig.SaveBehavior.UPDATE);
-
-        if (!isNullOrEmpty(this.movieRatingsTable)) {
-            writeToMovieRatingConfig.withTableNameOverride(new DynamoDBMapperConfig.TableNameOverride(this.movieRatingsTable));
-        }
-
-        this.dynamoDBMapper.save(movieRating, writeToMovieRatingConfig.build());
+        this.dynamoDBMapper.save(movieRating, writeToMovieRatingConfig);
     }
 }
