769b890e4adb8e7157078247ee3ec9fcda29efc7
Bogdan Mihai <bogdan-eugen.mihai@klarna.com>
2020-03-07 12:19:27 +0100

    Add dagger models

---
+++ java11/movies/update-movie-rating/src/main/java/de/mbe/tutorials/aws/serverless/movies/updatemovierating/config/FnModule.java
@@ -0,0 +1,47 @@
+package de.mbe.tutorials.aws.serverless.movies.updatemovierating.config;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.xray.AWSXRay;
+import com.amazonaws.xray.handlers.TracingHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dagger.Module;
+import dagger.Provides;
+import de.mbe.tutorials.aws.serverless.movies.updatemovierating.repository.MoviesDynamoDbRepository;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+@Module
+public class FnModule {
+
+    @Singleton
+    @Provides
+    @Named("movieRatingsTable")
+    String movieRatingsTable() {
+        return System.getenv("MOVIE_RATINGS_TABLE");
+    }
+
+    @Singleton
+    @Provides
+    AmazonDynamoDB amazonDynamoDB() {
+        return AmazonDynamoDBClientBuilder
+                .standard()
+                .withRequestHandlers(new TracingHandler(AWSXRay.getGlobalRecorder()))
+                .build();
+    }
+
+    @Singleton
+    @Provides
+    ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+
+    @Singleton
+    @Provides
+    public MoviesDynamoDbRepository moviesDynamoDbRepository(
+            final AmazonDynamoDB amazonDynamoDB,
+            @Named("movieRatingsTable") final String movieRatingsTable) {
+        return new MoviesDynamoDbRepository(amazonDynamoDB, movieRatingsTable);
+    }
+}
---
+++ java11/movies/upload-movie-infos/src/main/java/de/mbe/tutorials/aws/serverless/movies/uploadmovieinfos/config/FnModule.java
@@ -0,0 +1,58 @@
+package de.mbe.tutorials.aws.serverless.movies.uploadmovieinfos.config;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.xray.AWSXRay;
+import com.amazonaws.xray.handlers.TracingHandler;
+import dagger.Module;
+import dagger.Provides;
+import de.mbe.tutorials.aws.serverless.movies.uploadmovieinfos.repository.MoviesDynamoDbRepository;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+@Module
+public class FnModule {
+
+    @Singleton
+    @Provides
+    @Named("movieInfosTable")
+    String movieInfosTable() {
+        return System.getenv("MOVIE_INFOS_TABLE");
+    }
+
+    @Singleton
+    @Provides
+    @Named("movieInfosBucket")
+    String movieInfosBucket() {
+        return System.getenv("MOVIE_INFOS_BUCKET");
+    }
+
+    @Singleton
+    @Provides
+    AmazonDynamoDB amazonDynamoDB() {
+        return AmazonDynamoDBClientBuilder
+                .standard()
+                .withRequestHandlers(new TracingHandler(AWSXRay.getGlobalRecorder()))
+                .build();
+    }
+
+    @Singleton
+    @Provides
+    AmazonS3 amazonS3() {
+        return AmazonS3ClientBuilder
+                .standard()
+                .withRequestHandlers(new TracingHandler(AWSXRay.getGlobalRecorder()))
+                .build();
+    }
+
+    @Singleton
+    @Provides
+    public MoviesDynamoDbRepository moviesDynamoDbRepository(
+            final AmazonDynamoDB amazonDynamoDB,
+            @Named("movieInfosTable") final String movieInfosTable) {
+        return new MoviesDynamoDbRepository(amazonDynamoDB, movieInfosTable);
+    }
+}
